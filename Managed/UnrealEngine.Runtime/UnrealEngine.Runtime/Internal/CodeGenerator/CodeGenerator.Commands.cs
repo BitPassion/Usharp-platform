19cf7e095349762954583b7e4b440848a9063cf3
asherhoffman1120 <asher.hoffman1120@gmail.com>
2018-10-10 23:47:38 +0100

    Fix hotreload crash due to bad timing (changed from AsyncTask to FTicker)

--- Managed/UnrealEngine.Runtime/UnrealEngine.Runtime/Internal/CodeGenerator/CodeGenerator.Commands.cs
+++ Managed/UnrealEngine.Runtime/UnrealEngine.Runtime/Internal/CodeGenerator/CodeGenerator.Commands.cs
@@ -17,8 +17,6 @@ namespace UnrealEngine.Runtime
             //IConsoleManager.Get().RegisterConsoleCommand("USharpGenSliced", "USharp generate C# code", GenerateCodeTimeSliced);
 
             IConsoleManager.Get().RegisterConsoleCommand("USharpMinHotReload", "USharp hotreload will skip reintancing / CDO checks", SetMinimalHotReload);
-
-            IConsoleManager.Get().RegisterConsoleCommand("USharpCompileGeneratedSln", "USharp compile generated C# code", CompileGeneratedCode);
         }
 
         private static void SetMinimalHotReload(string[] args)
@@ -38,18 +36,6 @@ namespace UnrealEngine.Runtime
         private static void GenerateCode(string[] args)
         {
             GenerateCode(false, args);
-        }
-
-        private static void CompileGeneratedCode(string[] args)
-        {
-            CodeGenerator codeGenerator = new CodeGenerator(false);
-            CodeManager codeManager = CodeManager.Create(codeGenerator);
-            var _fileWriterCodeManager = (FileWriterCodeManager)codeManager;
-            if (_fileWriterCodeManager != null)
-            {
-                _fileWriterCodeManager.OnBeginGenerateModules();
-                _fileWriterCodeManager.AttemptToBuildGeneratedSolution();
-            }
         }
 
         private static void GenerateCodeTimeSliced(string[] args)
--- Managed/UnrealEngine.Runtime/UnrealEngine.Runtime/Internal/CodeGenerator/CodeManagers/FileWriterCodeManager.cs
+++ Managed/UnrealEngine.Runtime/UnrealEngine.Runtime/Internal/CodeGenerator/CodeManagers/FileWriterCodeManager.cs
@@ -142,45 +142,10 @@ namespace UnrealEngine.Runtime
             catch (Exception e)
             {
                 Log(ELogVerbosity.Error, e.Message, e);
-                return;
             }
             finally
             {
                 Log(ELogVerbosity.Display, "Done Generating Modules, Solution is at " + modulesSlnPath);
-            }
-        }
-
-        public void AttemptToBuildGeneratedSolution()
-        {
-            string _slnPath = Path.Combine(Settings.GetManagedModulesDir(), "UnrealEngine.sln");
-            string _projPath = Path.Combine(Settings.GetManagedModulesDir(), "UnrealEngine.csproj");
-            if (!File.Exists(_slnPath))
-            {
-                Log(ELogVerbosity.Error, "Can't Compile: The Solution " + _slnPath + " doesn't exist");
-                return;
-            }
-            if (!File.Exists(_projPath))
-            {
-                Log(ELogVerbosity.Error, "Can't Compile: The Project " + _projPath + " doesn't exist");
-                return;
-            }
-            Log(ELogVerbosity.Display, "Attempting To Build Generated Solution at " + _slnPath);
-            bool shippingBuild = false;
-            bool x86Build = false;
-            string customDefines = null;
-            if (shippingBuild)
-            {
-                // This is to clear the editor defines (WITH_EDITORONLY_DATA) which gives us a runtime FName struct
-                customDefines = "BLANK_DEFINES";
-            }
-            if (!BuildCs(_slnPath, _projPath, !shippingBuild, x86Build, customDefines))
-            {
-                Log(ELogVerbosity.Display, "Failed to build (see build.log) - " + Path.GetFileName(modulesProjPath));
-                return;
-            }
-            else
-            {
-                Log(ELogVerbosity.Display, "Build successful - " + Path.GetFileName(_projPath));
             }
         }
 
