dc59b8e4647b648ff65d378f76da161a819945b4
asherhoffman1120 <asher.hoffman1120@gmail.com>
2018-10-31 09:36:45 +0000

    Fix FLinearColor tables array initialization

--- Managed/UnrealEngine.Runtime/UnrealEngine.Runtime/InjectedClasses/Engine/Actor_Injected.cs
+++ Managed/UnrealEngine.Runtime/UnrealEngine.Runtime/InjectedClasses/Engine/Actor_Injected.cs
@@ -1,4 +1,4 @@
-﻿using UnrealEngine.Runtime;
+using UnrealEngine.Runtime;
 using UnrealEngine.Runtime.Native;
 
 namespace UnrealEngine.Engine
@@ -9,11 +9,6 @@ namespace UnrealEngine.Engine
         public UWorld World
         {
             get { return worldCached.Update(Native_AActor.GetWorld(Address)); }
-        }
-
-        public void PrintString(string InString, FLinearColor TextColor, bool PrintToLog = false, float Duration = 1f)
-        {
-            USystemLibrary.PrintString(GCHelper.Find<AActor>(Address), InString, true, PrintToLog, TextColor, Duration);
         }
     }
 }
